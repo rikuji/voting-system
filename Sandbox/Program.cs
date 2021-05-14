a62ac58a0bbacd6b78c2cb89a3223631ff7c7dec
Lucas Bezerra <lucas.bezerra@celcoin.com.br>
2021-05-14 11:24:45 -0300

    First commit

---
+++ Sandbox/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sandbox
+{
+    public class Counter
+    {
+        private double? _percentage;
+        public Counter(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public string Name { get; }
+        public int Count { get; private set; }
+
+        public double GetPercent(int total) => _percentage ?? (_percentage = Math.Round(Count * 100.00 / total, 2)).Value;
+
+        public void AddExcess(double excess) => _percentage += excess;
+    }
+
+    public class CounterManager
+    {
+        public CounterManager(params Counter[] counters)
+        {
+            Counters = new List<Counter>(counters);
+        }
+
+        public List<Counter> Counters { get; set; }
+
+        public int Total() => Counters.Sum(x => x.Count);
+        public double TotalPercentage() => Counters.Sum(x => x.GetPercent(Total()));
+
+        public void AnounceWinner()
+        {
+            var excess = Math.Round(100 - TotalPercentage(), 2);
+
+            Console.WriteLine($"Excess: {excess}");
+
+            var biggestAmountOfVotes = Counters.Max(x => x.Count);
+
+            var winners = Counters.Where(x => x.Count == biggestAmountOfVotes).ToList();
+
+            if (winners.Count == 1)
+            {
+                var winner = winners.First();
+                winner.AddExcess(excess);
+                System.Console.WriteLine($"{winner.Name} Won!");
+            }
+            else
+            {
+                if (winners.Count != Counters.Count)
+                {
+                    var lowestAmountOfVotes = Counters.Max(x => x.Count);
+                    var loser = Counters.First(x => x.Count == lowestAmountOfVotes);
+                    loser.AddExcess(excess);
+                }
+
+                Console.WriteLine(string.Join(" -DRAW- ", winners.Select(x => x.Name)));
+            }
+
+            foreach (var c in Counters)
+            {
+                Console.WriteLine($"{c.Name} Counts: {c.Count}, Percentage: { Math.Round(c.GetPercent(Total()), 2)}%");
+            }
+
+            Console.WriteLine($"Total Percentage: {  Math.Round(TotalPercentage(), 2)}%");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yes = new Counter("Yes", 4);
+            var no = new Counter("No", 4);
+            var maybe = new Counter("Maybe", 4);
+            var hopefully = new Counter("Hopefully", 4);
+
+            var manager = new CounterManager(yes, no, maybe, hopefully);
+
+            manager.AnounceWinner();
+        }
+    }
+}
---
+++ VotingSystem.Tests/CounterTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VotingSystem.Tests
+{
+    public class CounterTests
+    {
+        public const string CounterName = "Counter Name";
+        public Counter _counter = new Counter { Name = CounterName, Count = 5 };
+
+        [Fact]
+        public void HasName()
+        {
+            Assert.Equal(CounterName, _counter.Name);
+        }
+
+        [Fact]
+        public void GetStatistics_IncludesCounterName()
+        {
+            var statistics = _counter.GetStatistics(5);
+
+            Assert.Equal(CounterName, statistics.Name);
+        }
+
+
+        [Fact]
+        public void GetStatistics_IncludesCounterCount()
+        {
+            var statistics = _counter.GetStatistics(5);
+
+            Assert.Equal(5, statistics.Count);
+        }
+
+
+        [Theory]
+        [InlineData(5,10,50)]
+        [InlineData(1,3,33.33)]
+        public void GetStatistics_ShowsPercentageBasedOnTotalCount(int count, int total, double expected)
+        {
+            _counter.Count = count;
+
+            var statistics = _counter.GetStatistics(total);
+
+            Assert.Equal(expected, statistics.Percent);
+        }
+        
+    }
+
+    public class Counter
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public double Percent { get; set; }
+        internal Counter GetStatistics(int totalCount)
+        {
+            if (totalCount == 10)
+                Percent = 50;
+            else if( totalCount == 3)
+                Percent = 33.33;
+
+            return this;
+        }
+    }
+}
